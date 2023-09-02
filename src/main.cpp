bca4fa6c5eebdcf52eb4f8d8ec09340fa52bb081
Rentoo <dreamYiZ@outlook.com>
2023-09-02 14:38:06 +0800

    -

--- src/main.cpp
+++ src/main.cpp
@@ -37,33 +37,89 @@ int LED_PIN_3 = 8;
 */
 
 // int led = 9;         // the PWM pin the LED is attached to
-int brightness = 0;  // how bright the LED is
-int fadeAmount = 5;  // how many points to fade the LED by
+int brightness = 0; // how bright the LED is
+int fadeAmount = 5; // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
-void setup() {
+void setup()
+{
   // declare pin 9 to be an output:
   // pinMode(led, OUTPUT);
 
-   pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
   pinMode(LED_PIN_2, OUTPUT);
   pinMode(LED_PIN_3, OUTPUT);
 }
+/*
+  Blink without Delay
 
-// the loop routine runs over and over again forever:
-void loop() {
-  // set the brightness of pin 9:
-  analogWrite(LED_PIN, brightness);
-  analogWrite(LED_PIN_2, brightness);
-  analogWrite(LED_PIN_3, brightness);
+  Turns on and off a light emitting diode (LED) connected to a digital pin,
+  without using the delay() function. This means that other code can run at the
+  same time without being interrupted by the LED code.
 
-  // change the brightness for next time through the loop:
-  brightness = brightness + fadeAmount;
+  The circuit:
+  - Use the onboard LED.
+  - Note: Most Arduinos have an on-board LED you can control. On the UNO, MEGA
+    and ZERO it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN
+    is set to the correct LED pin independent of which board is used.
+    If you want to know what pin the on-board LED is connected to on your
+    Arduino model, check the Technical Specs of your board at:
+    https://www.arduino.cc/en/Main/Products
 
-  // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
-    fadeAmount = -fadeAmount;
+  created 2005
+  by David A. Mellis
+  modified 8 Feb 2010
+  by Paul Stoffregen
+  modified 11 Nov 2013
+  by Scott Fitzgerald
+  modified 9 Jan 2017
+  by Arturo Guadalupi
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/BlinkWithoutDelay
+*/
+
+// constants won't change. Used here to set a pin number:
+const int ledPin = LED_BUILTIN; // the number of the LED pin
+
+// Variables will change:
+int ledState = LOW; // ledState used to set the LED
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0; // will store last time LED was updated
+
+// constants won't change:
+const long interval = 1000; // interval at which to blink (milliseconds)
+
+void loop()
+{
+  // here is where you'd put code that needs to be running all the time.
+
+  // check to see if it's time to blink the LED; that is, if the difference
+  // between the current time and last time you blinked the LED is bigger than
+  // the interval at which you want to blink the LED.
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval)
+  {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW)
+    {
+      ledState = HIGH;
+    }
+    else
+    {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(LED_PIN, ledState);
+    digitalWrite(LED_PIN_2, ledState);
+    digitalWrite(LED_PIN_3, ledState);
   }
-  // wait for 30 milliseconds to see the dimming effect
-  delay(30);
 }
